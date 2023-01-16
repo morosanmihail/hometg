b9e00023e74a894c99b992d4009e8fe38245189c
Mihail Morosan <morosanmihail@outlook.com>
2023-01-16 18:21:15 +0000

    Auto-download of MTGJSON.com AllPrintings.

--- HomeTGCollection/Program.cs
+++ HomeTGCollection/Program.cs
@@ -2,6 +2,10 @@ using Microsoft.EntityFrameworkCore;
 using HomeTG.Models;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Data.Sqlite;
+using System.Security.Cryptography;
+using System.Net;
+using System;
+using System.IO;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +21,7 @@ var incomingSQL = "create table if not exists incoming(uuid string, quantity int
 CreateDBIfNotExists(@"DB\Collection.db", collectionSQL);
 CreateDBIfNotExists(@"DB\Incoming.db", incomingSQL);
 // Add download of newer AllPrintings.db here
+DownloadPrintingsDBIfNotExists(@"https://mtgjson.com/api/v5/AllPrintings.sqlite", @"DB\AllPrintings.db");
 
 var connectionString = builder.Configuration.GetConnectionString("MtgJson");
 builder.Services.AddDbContext<DB>(options => options.UseSqlite(connectionString));
@@ -47,6 +52,53 @@ static void CreateDBIfNotExists(string DB, string sql)
             sqlite.Open();
             SqliteCommand command = new SqliteCommand(sql, sqlite);
             command.ExecuteNonQuery();
+        }
+    }
+}
+
+static void DownloadPrintingsDBIfNotExists(string DBURL, string LocalFile)
+{
+    bool download = false;
+
+    string remoteHash = "";
+    using (WebClient client = new WebClient())
+    {
+        client.DownloadFile(DBURL + ".sha256", LocalFile + ".sha256");
+        remoteHash = File.ReadAllText(LocalFile + ".sha256");
+    }
+
+    string localHash = "";
+    if (!File.Exists(LocalFile))
+    {
+        download = true;
+    }
+    else
+    {
+        localHash = GetSHA256HashFromFile(LocalFile);
+        if(remoteHash != localHash)
+        {
+            download = true;
+        }
+    }
+
+    if (download) {
+        Console.WriteLine("File is NOT up to date. Downloading...");
+        using (WebClient client = new WebClient())
+        {
+            client.DownloadFile(DBURL, LocalFile);
+        }
+        Console.WriteLine("Downloaded.");
+    }
+}
+
+static string GetSHA256HashFromFile(string filePath)
+{
+    using (FileStream stream = File.OpenRead(filePath))
+    {
+        using (SHA256 sha = SHA256.Create())
+        {
+            byte[] hash = sha.ComputeHash(stream);
+            return BitConverter.ToString(hash).Replace("-", "").ToLower();
         }
     }
 }
