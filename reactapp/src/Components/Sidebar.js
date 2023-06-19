36a6554e66350a257d6dc3a1ff7c734e9982340f
Mihail Morosan <morosanmihail@outlook.com>
2023-06-19 18:03:52 +0100

    Improve sidebar slightly.

--- reactapp/src/Components/Sidebar.js
+++ reactapp/src/Components/Sidebar.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link, useParams } from "react-router-dom";
 
 function Sidebar() {
-    const { collection } = useParams();
+    const { collection = "Main" } = useParams();
     const [collections, setCollections] = useState([]);
     const [loading, setLoading] = useState(true);
 
@@ -19,13 +19,11 @@ function Sidebar() {
 
     const renderCollections = (collections) => {
         return (
-            <div className="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-                {collections.map(c =>
-                    <Link to={"/" + c.id} key={c.id}>
-                        <button className={"nav-link" + (c.id === collection ? " active" : "")} type="button">{c.id}</button>
-                    </Link>
-                )}
-            </div>
+            collections.map(c =>
+                <Link to={"/" + c.id} key={c.id} className={"nav-link" + (c.id === collection ? " active" : "")}>
+                    {c.id}
+                </Link>
+            )
         );
     }
 
@@ -35,21 +33,23 @@ function Sidebar() {
 
     return (
         <header>
-        <nav id="sidebarMenu" className="collapse d-lg-block sidebar collapse bg-white">
-                    <div className="position-sticky">
-                        <div className="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-                            <button className="btn btn-default" type="button" data-toggle="collapse" data-target="#search">Search</button>
-                        </div>
-
-                    {contents}
+            <nav id="sidebarMenu" className="collapse d-lg-block sidebar collapse bg-white">
+                <div className="position-sticky">
+                    <div className="nav flex-column nav-pills me-3" role="tablist" aria-orientation="vertical">
+                        <button className="btn btn-default" type="button" data-toggle="collapse" data-target="#search">Search</button>
                     </div>
-                </nav>
-                <nav id="main-navbar" className="navbar navbar-expand-lg navbar-light bg-white fixed-top">
-                    <div className="container-fluid">
-                        <a className="navbar-brand" href="/">
-                            HomeTG
-                        </a>
+                    <hr/>
+                    <div className="nav flex-column nav-pills me-3" role="tablist" aria-orientation="vertical">
+                        {contents}
                     </div>
+                </div>
+            </nav>
+            <nav id="main-navbar" className="navbar navbar-expand-lg navbar-light bg-white fixed-top">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="/">
+                        HomeTG
+                    </a>
+                </div>
             </nav>
         </header>
     );
--- reactapp/src/setupProxy.js
+++ reactapp/src/setupProxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const context = [
-    "/mtg/cards",
+    "/mtg",
     "/collection",
 ];
 
