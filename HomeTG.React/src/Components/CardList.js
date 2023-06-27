b0b1353b1fc036da12b8b93f444182bf067e3a15
Mihail Morosan <morosanmihail@outlook.com>
2023-06-27 13:33:41 +0100

    Added proper pagination to Search.

--- HomeTG.React/src/Components/CardList.js
+++ HomeTG.React/src/Components/CardList.js
@@ -93,24 +93,17 @@ export default function CardList({ showSearch=false }) {
                         }
                     </div>
                     <ReactPaginate
-                        previousLabel="Previous"
-                        nextLabel="Next"
-                        pageClassName="page-item"
-                        pageLinkClassName="page-link"
-                        previousClassName="page-item"
-                        previousLinkClassName="page-link"
-                        nextClassName="page-item"
-                        nextLinkClassName="page-link"
-                        breakLabel="..."
-                        breakClassName="page-item"
-                        breakLinkClassName="page-link"
+                        previousLabel="Previous" nextLabel="Next"
+                        pageClassName="page-item" pageLinkClassName="page-link"
+                        previousClassName="page-item" previousLinkClassName="page-link"
+                        nextClassName="page-item" nextLinkClassName="page-link"
+                        breakLabel="..." breakClassName="page-item" breakLinkClassName="page-link"
+                        containerClassName="pagination" activeClassName="active"
                         pageCount={Math.ceil(parseInt(cardCount) / pageSize)}
                         marginPagesDisplayed={2}
                         pageRangeDisplayed={5}
                         onPageChange={handlePageChange}
-                        containerClassName="pagination"
-                        activeClassName="active"
-                        forcePage={Math.max(0, pageNumber - 1)}
+                        forcePage={(cardCount > 0) ? Math.max(0, pageNumber - 1) : -1}
                     />
                 </RefreshCardListContext.Provider>
             </CardLoaderProvider>
--- HomeTG.React/src/Components/Search.js
+++ HomeTG.React/src/Components/Search.js
@@ -5,9 +5,10 @@ import { useOperations } from '../OperationsContext';
 import ReactPaginate from "react-paginate";
 
 function Search({ dedicatedPage = false, onAdd }) {
+    const ops = useOperations();
     const [cards, setCards] = useState([]);
     const [loading, setLoading] = useState(false);
-    const [offset, setOffset] = useState(0);
+    const [pageNumber, setPageNumber] = useState(1);
     const [shouldSearch, setShouldSearch] = useState(dedicatedPage);
 
     const [searchParams, setSearchParams] = useSearchParams();
@@ -21,45 +22,28 @@ function Search({ dedicatedPage = false, onAdd }) {
 
     let pageSize = 24;
 
-    const ops = useOperations();
-
-    const renderCards = (cards) => {
-        return (
-            <div className="card-grid list">
-                {cards.map(card =>
-                    <Card key={card.id} id={card.id} card={card} onAdd={onAdd} />
-                )}
-            </div>
-        );
-    }
-
     useEffect(() => {
         if(shouldSearch) {
-            populateSearch(offset);
+            setLoading(true);
+
+            ops.fetch(
+                "Searching the MtG database", [],
+                '/mtg/cards/search?pageSize=' + pageSize + '&offset=' + ((pageNumber-1) * pageSize),
+                {
+                    method: "post",
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(searchOptions)
+                }
+            ).then(data => {
+                setCards(data);
+                setLoading(false);
+                setShouldSearch(false);
+            })
         }
-    }, [offset, shouldSearch])
-
-    const populateSearch = () => {
-        setLoading(true);
-
-        ops.fetch("Searching the MtG database", [], '/mtg/cards/search?pageSize=' + pageSize + '&offset=' + offset, {
-            method: "post",
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(searchOptions)
-        }).then(data => {
-            setCards(data);
-            setLoading(false);
-            setShouldSearch(false);
-        })
-    }
-
-    const triggerSearch = (e, newOffset) => {
-        setShouldSearch(true);
-        setOffset(newOffset);
-    }
+    }, [pageNumber, shouldSearch])
 
     const handleSearchInput = (event, field) => {
         let newState = Object.assign({}, searchOptions);
@@ -68,9 +52,10 @@ function Search({ dedicatedPage = false, onAdd }) {
         setSearchParams(newState);
     }
 
-    let contents = loading
-        ? <p>Loading...</p>
-        : renderCards(cards);
+    const handlePageChange = (event) => {
+        setShouldSearch(true);
+        setPageNumber(parseInt(event.selected)+1);
+    };
 
     return (
         <React.Fragment>
@@ -92,28 +77,31 @@ function Search({ dedicatedPage = false, onAdd }) {
                         <button onClick={event => setShouldSearch(true)} className="btn btn-outline-secondary" type="button" id="button-addon2">Search</button>
                     </div>
                     <div className="search-results" id="search-results">
-                        {contents}
+                        {
+                            loading ?
+                            <p>Loading...</p>
+                            :
+                            <div className="card-grid list">
+                                {cards.map(card =>
+                                    <Card key={card.id} id={card.id} card={card} onAdd={onAdd} />
+                                )}
+                            </div>
+                        }
                     </div>
                         { cards.length > 0 ?
-                    <nav aria-label="Page navigation">
-                        <ul className="pagination center">
-                            <li className={"page-item" + (parseInt(offset) === 0 ? " disabled" : "")}>
-                                {
-                                    parseInt(offset) > 0 ?
-                                    <button onClick={e => triggerSearch(e, parseInt(offset) - pageSize)} className="page-link">Previous</button>
-                                : <button className="page-link">Previous</button>}
-                            </li>
-                            <li className="page-item disabled">
-                                <button id="list-page" className="page-link">{(parseInt(offset) + pageSize) / pageSize}</button>
-                            </li>
-                            <li className={"page-item" + (cards.length < pageSize ? " disabled" : "")}>
-                                {
-                                    cards.length >= pageSize ?
-                                    <button onClick={e => triggerSearch(e, parseInt(offset) + pageSize)}  className="page-link">Next</button>
-                                : <button className="page-link">Next</button>}
-                            </li>
-                        </ul>
-                    </nav>
+                    <ReactPaginate
+                        previousLabel="Previous" nextLabel="Next"
+                        pageClassName="page-item" pageLinkClassName="page-link"
+                        previousClassName="page-item" previousLinkClassName="page-link"
+                        nextClassName="page-item" nextLinkClassName="page-link"
+                        breakLabel="..." breakClassName="page-item" breakLinkClassName="page-link"
+                        containerClassName="pagination" activeClassName="active"
+                        pageCount={(cards.length === pageSize) ? pageNumber + 1 : pageNumber}
+                        marginPagesDisplayed={2}
+                        pageRangeDisplayed={5}
+                        onPageChange={handlePageChange}
+                        forcePage={(cards.length > 0) ? Math.max(0, pageNumber - 1) : -1}
+                    />
             : null }
                 </div>
                 <hr />
